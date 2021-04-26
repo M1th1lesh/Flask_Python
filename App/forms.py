dabf94c71402200487a68c37ba18a231f5a736ec
M1th1lesh <mithileshchaudhari2828@gmail.com>
2021-04-26 17:24:44 +0530

    Profile/account page , update form with image

--- App/forms.py
+++ App/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField,PasswordField , SubmitField ,BooleanField, ValidationError
 #validationError is used to send out validation error messages
 #to import string fields and passowrd fields in froom as fields will be strings , Submit fields to submit teh ddata
@@ -8,7 +9,7 @@ from App.models import User
 from wtforms.validators import DataRequired ,Length  ,EqualTo ,Email
 # for data required in fill validation , length for length validation
 #emial for email validation , Equalto for comparing strings
-
+from flask_login import current_user
 
 # to create registration forms in flask
 class RegistrationForm(FlaskForm):
@@ -48,3 +49,28 @@ class LoginForm(FlaskForm):
     remember = BooleanField('Remember Me')
 
     submit = SubmitField('Log in')
+
+# to create update forms forms in flask
+class UpdateAccoutForm(FlaskForm):
+    #variable = field('lables',valdations)
+    #form fields
+    username = StringField('Username',
+                            validators=[DataRequired(),Length(min=2,max=20)] )
+    #setting conditions/validators for user name
+    email = StringField('Email',
+                        validators=[DataRequired(),Email()])
+    picture = FileField('Update Profile picture' ,validators=[FileAllowed(['jpg','png'])])
+    submit = SubmitField('Update')
+
+    #these functions are created so that we can throw error before commiting to database .These will validate the fields for errors 
+    #of duplicated entries of user_name or email
+    def validate_username(self,username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first() 
+            if user : 
+                raise ValidationError('This username is taken . Please user different one')
+    def validate_email(self,email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first() 
+            if user : 
+                raise ValidationError('This email is taken . Please user different one')
--- App/routes.py
+++ App/routes.py
@@ -2,9 +2,12 @@ from flask import render_template,url_for,flash,redirect , request #
 from App import app , db , bcrypt #app,db , bcrypt is imported from teh __init__ file
 #flash is used to give one time alerts , url_for is used to find methods/pages on it's own
 #redirect to move to another page
-from App.forms import RegistrationForm,LoginForm  #importing  classes from our forms.py
+from App.forms import RegistrationForm,LoginForm,UpdateAccoutForm  #importing  classes from our forms.py
 from App.models import User, Post
 from flask_login import login_user,current_user,logout_user ,login_required
+import secrets
+import os #for path functionality
+from PIL import Image
 
 
 posts= [
@@ -79,7 +82,40 @@ def logout():
     print("You are logged out ")
     return redirect(url_for('home'))
 
-@app.route("/account")
+
+# Saving Image 
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _,f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path,'static/profile_pics',picture_fn)
+    
+    output_size = (125,125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+    return picture_fn
+
+
+
+#Accoutn 
+@app.route("/account", methods= ['GET','POST'])
 @login_required   #to keep a check for log in required
 def account():
-    return render_template('account.html',title='Account')
+    form = UpdateAccoutForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+
+        current_user.username = form.username.data 
+        current_user.email = form.email.data
+        db.session.commit()
+        print("Account updated")
+        flash('Your account has been updated','success')
+        return redirect(url_for('account'))
+    elif request.method=='GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for('static',filename='profile_pics/'+current_user.image_file)
+    return render_template('account.html',title='Account',image_file =  image_file, form = form)
